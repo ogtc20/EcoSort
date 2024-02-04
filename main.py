879b4440a3f88d4601c370a7b8be9ea7f2458a2e
ogtc20 <owengtchung@outlook.com>
2024-02-03 21:48:23 -0500

    Azure AI Computer Vision - Attempt 2

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ project_id = os.getenv('ProjectID')
 model_name = os.getenv('ModelName')
 
 prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
-predictor = CustomVisionPredictionClient(endpoint=prediction_endpoint, credentials=prediction_credentials)
+prediction_client = CustomVisionPredictionClient(endpoint=prediction_endpoint, credentials=prediction_credentials)
 
-img_url1 = 'https://sushiumi.ca/cdn/shop/products/20210406_173613_530x@2x.jpg?v=1617745009'
-response = predictor.classify_image_url(project_id, model_name, img_url1)
+for image in os.listdir('test-images'):
+    image_data = open(os.path.join('test-images',image), "rb").read()
+    results = prediction_client.classify_image(project_id, model_name, image_data)
 
-print(response.deserialize)
-
+    # Loop over each label prediction and print any with probability > 50%
+    for prediction in results.predictions:
+          if prediction.probability > 0.5:
+              print('{} ({:.0%})'.format(prediction.tag_name, prediction.probability))
