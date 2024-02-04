8a5f42ad6f0de54fad45c034d533df187c52e6fd
ogtc20 <owengtchung@outlook.com>
2024-02-04 00:37:56 -0500

    Azure AI Computer Vision - Working with singular image

--- main.py
+++ main.py
@@ -10,11 +10,10 @@ model_name = os.getenv('ModelName')
 prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
 prediction_client = CustomVisionPredictionClient(endpoint=prediction_endpoint, credentials=prediction_credentials)
 
-for image in os.listdir('test-images'):
-    image_data = open(os.path.join('test-images',image), "rb").read()
-    results = prediction_client.classify_image(project_id, model_name, image_data)
-
-    # Loop over each label prediction and print any with probability > 50%
-    for prediction in results.predictions:
-          if prediction.probability > 0.5:
-              print('{} ({:.0%})'.format(prediction.tag_name, prediction.probability))
+image_data = open('PlasticWaterBottle.jpg', "rb").read()
+results = prediction_client.classify_image(project_id, model_name, image_data)
+ 
+# Loop over each label prediction and print any with probability > 50%
+for prediction in results.predictions:
+       if prediction.probability > 0.5:
+           print('{} ({:.0%})'.format(prediction.tag_name, prediction.probability))
